abea988544902d37b3b0c0920a9d8216c40f9c67
shiblu <mosabbir.shiblu@gmail.com>
2019-12-18 11:59:44 -0500

    Add comments

--- src/generics/pair/Pair.java
+++ src/generics/pair/Pair.java
@@ -6,6 +6,10 @@ public class Pair<T extends Number> {
 		System.out.println(num);
 	}
 
+	/**
+	 * U is not parameterized in the class before so had to be declared before the
+	 * method signature. T is already declared so no need to declare it again
+	 */
 	public <U extends Number> void add(T x, U y) {
 		System.out.println(x.doubleValue() + y.doubleValue());
 	}
@@ -13,7 +17,6 @@ public class Pair<T extends Number> {
 	public static void main(String[] args) {
 		Pair<Integer> pair = new Pair<>();
 		pair.print(10);
-
 		pair.add(10, 20);
 	}
 }
